d61bff795ed120fd9efd51ad9f35660014d3a820
Vagnerzezo <57641762+vagnerzezo@users.noreply.github.com>
2025-04-09 20:46:08 -0300

    Update index.vue

--- components/Rodape/index.vue
+++ components/Rodape/index.vue
@@ -11,13 +11,13 @@
         <img src="/img/logo/whatsApp.svg" alt="KR Representações" class="mx-auto w-8" />
        </a>
        <a href="#" aria-label="Instagram">
-        <img src="/img/logo/Instagram.svg" alt="KR Representações" class="mx-auto w-8" />
+        <img src="/img/logo/instagram.svg" alt="KR Representações" class="mx-auto w-8" />
        </a>
        <a href="#" aria-label="Facebook">
-        <img src="/img/logo/Facebook.svg" alt="KR Representações" class="mx-auto w-8" />
+        <img src="/img/logo/facebook.svg" alt="KR Representações" class="mx-auto w-8" />
        </a>
        <a href="#" aria-label="LinkedIn">
-        <img src="/img/logo/LinkedIn.svg" alt="KR Representações" class="mx-auto w-8" />
+        <img src="/img/logo/linkedIn.svg" alt="KR Representações" class="mx-auto w-8" />
        </a>
      </div>
 
